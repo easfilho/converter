07992bc5ac51a87dd2531f9e7bb3edb8c25222f2
Edson Andrade <edson_andrade_filho@hotmail.com>
2018-06-30 22:23:32 -0300

    refactory correction points of sonar

--- src/main/java/br/ilegratest/service/converterdata/ConverterData.java
+++ src/main/java/br/ilegratest/service/converterdata/ConverterData.java
@@ -3,6 +3,6 @@ package br.ilegratest.service.converterdata;
 import br.ilegratest.model.Data;
 
 public abstract class ConverterData {
-	protected final String SEPARATOR_CHARACTER = "ç";
+	protected static final String SEPARATOR_CHARACTER = "ç";
 	public abstract Data convert(String data);
 }
